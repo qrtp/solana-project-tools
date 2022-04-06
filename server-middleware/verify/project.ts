98c64725e5339bbd9652496aec24fa00e8e10e94
SolBots <97853296+SolBotsCo@users.noreply.github.com>
2022-04-06 12:48:55 +0100

    Update project.ts

--- server-middleware/verify/project.ts
+++ server-middleware/verify/project.ts
@@ -1,5 +1,5 @@
-import { list, read } from '../storage/persist'
-import { getConfigFilePath, getHodlerFilePath } from '../verify/paths'
+import { list, read, remove } from '../storage/persist'
+import { getConfigFilePath, getHodlerFilePath, getPublicKeyFilePath, getSalesAuditFilePath, getSalesFilePath, getVoteFilePath } from '../verify/paths'
 const loggerWithLabel = require('../logger/structured')
 
 /**
@@ -29,6 +29,53 @@ export async function getAllProjects() {
         }
     }
     return projectNames
+}
+
+// removes a project by name
+export async function removeProject(name: any) {
+
+    try {
+
+        // retrieve the project config
+        var config = await getConfig(name)
+        if (!config) {
+            logger.info(`project not found: ${name}`)
+            return false
+        }
+
+        // extract data we need for the delete
+        var updateAuthority = config.update_authority
+        if (!updateAuthority || updateAuthority == "") {
+            logger.info(`unable to find update authority for ${name}`)
+            return false
+        }
+        var ownerPublicKey = config.owner_public_key
+        if (!ownerPublicKey || ownerPublicKey == "") {
+            logger.info(`unable to find ownder public key for ${name}`)
+            return false
+        }
+
+        // log what we are about to remove
+        logger.info(`removing project ${name} with owner key ${ownerPublicKey} and update authority ${updateAuthority}`)
+
+        // remove all voting entries
+        var voteResults = await list("./config", `vote-results-${name}-`)
+        for (var i = 0; i < voteResults.length; i++) {
+            await remove(voteResults[i])
+        }
+
+        // remove all associated files
+        await remove(getConfigFilePath(name))
+        await remove(getHodlerFilePath(name))
+        await remove(getVoteFilePath(name))
+        await remove(getSalesFilePath(updateAuthority))
+        await remove(getSalesAuditFilePath(updateAuthority))
+        await remove(getPublicKeyFilePath(ownerPublicKey))
+        return true
+    } catch (e) {
+        logger.info(`error removing project ${name}`, e)
+    }
+    return false
 }
 
 // retreives the current hodler list in JSON format
